a92172b5ba9915a0ce73f2731ae14d8adb0ca81a
Michael Bieri <32072807+chline1337@users.noreply.github.com>
2025-03-18 22:05:47 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Esports Platform MVP</h1>
+            <p>Welcome to the platform!</p>
+        </div>
+    );
+}
+
+export default App;
